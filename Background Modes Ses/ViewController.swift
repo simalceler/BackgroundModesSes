e403e0430d47423fa40ff47bb5cb00408366aea9
simalceler <celersimal@gmail.com>
2023-05-23 12:14:10 +0300

    git add.

--- Background Modes Ses/ViewController.swift
+++ Background Modes Ses/ViewController.swift
@@ -6,14 +6,30 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    
+    var sesOynatici:AVAudioPlayer!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        do{
+            let dosyaYolu = Bundle.main.path(forResource: "music", ofType: ".mp3")
+            sesOynatici = try AVAudioPlayer(contentsOf: URL.init(fileURLWithPath: dosyaYolu!))
+            sesOynatici.prepareToPlay()
+        }catch{
+            print(error.localizedDescription)
+        }
     }
 
-
+    @IBAction func basla(_ sender: Any) {
+        sesOynatici.play()
+    }
+    
+    @IBAction func dur(_ sender: Any) {
+        sesOynatici.stop()
+    }
 }
 
